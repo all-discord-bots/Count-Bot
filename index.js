f569e670613325fefa4a48a106228d99384cba21
BannerBomb <BannerBomb55@gmail.com>
2018-01-18 23:03:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ bot.on('ready', () => {
   console.info('Counting bot connected');
   bot.editStatus("online", { name: "1 2 3...", type: 0 });
 });
-bot.on('disconnected', () => console.warn('Counting bot disconnected'));
+
+bot.on('disconnected', () => {
+  console.warn('Counting bot disconnected');
+});
 
 bot.once('ready', () => {
 	return bot.guilds.forEach(g => {
@@ -33,16 +36,20 @@ bot.on('channelCreate', channel => {
 
 bot.on('messageCreate', message => {
 	if (countingChannels.has(message.channel.id))
+	if (message.channel.id !== 381974306693054476) return;
 	  if (message.author.bot) return; // dont do anything if message is from bot
 		  return countingChannels.get(message.channel.id).handleNewMessage(message);
 });
 
-//bot.on('messageDelete', message => {
+bot.on('messageDelete', message => {
 //  if (countingChannels.has(message.channel.id)) {
 //    message.channel.guild.members.get(message.author.id).addRole("381975847977877524");
 //  }
   //return countingChannels.get(message.channel.id).handleDelMessage(message);
-    //bot.createMessage(message.channel.id, message.content);
-//});
+  var gusername = message.author.name;
+  var gdiscrim = message.author.discriminator;
+  var guser = gusername + '#' + gdiscrim;
+    bot.createMessage(403757067225006101, guser + ' ' + message.content);
+});
 
 bot.connect();
