11b74f51ae1840f87347eea4a14dad63a9362758
BannerBomb <BannerBomb55@gmail.com>
2018-10-11 21:12:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config.json'),
 	Eris = require('eris'),
-	bot = new Eris(config.token, {
+	bot = new Eris(process.env.BOT_TOKEN, { // config.token
 		messageLimit: 0
 	}),
 	CountingChannelManager = require('./CountingChannelManager');
@@ -19,8 +19,8 @@ bot.on('disconnected', () => {
 });
 
 bot.once('ready', () => {
-	return bot.guilds.forEach(g => {
-		return g.channels.filter(c => c.name.startsWith('counting')).forEach(c => {
+	return bot.guilds.forEach((g) => {
+		return g.channels.filter((c) => c.name.startsWith('counting')).forEach((c) => {
 			let manager = new CountingChannelManager(c);
 			manager.init();
 			return countingChannels.set(c.id, manager);
@@ -28,7 +28,7 @@ bot.once('ready', () => {
 	});
 });
 
-bot.on('channelCreate', channel => {
+bot.on('channelCreate', (channel) => {
 	if (channel.name.startsWith('counting')) {
 		let manager = new CountingChannelManager(channel);
 		manager.init();
@@ -36,25 +36,25 @@ bot.on('channelCreate', channel => {
 	}
 });
 
-bot.on('messageCreate', message => {
+bot.on('messageCreate', (message) => {
+	if (message.author.bot) return; // dont do anything if message is from bot
 	if (message.channel.id == "410125427777077248") return;
-	if (countingChannels.has(message.channel.id))
-//	if (message.channel.id !== "381974306693054476") return;
-	  if (message.author.bot) return; // dont do anything if message is from bot
-		  return countingChannels.get(message.channel.id).handleNewMessage(message);
+	if (countingChannels.has(message.channel.id)) {
+		return countingChannels.get(message.channel.id).handleNewMessage(message);
+	}
 });
 
 process.on("uncaughtException", (err) => {
-    bot.emit("error", err)
-    process.exit(1)
+	bot.emit("error", err)
+	process.exit(1)
 });
 
 process.on("unhandledRejection", (err) => {
-    bot.emit("error", err)
+	bot.emit("error", err)
 });
 
 bot.on("error", (err) => {
-    console.warn(err.stack);
+	console.warn(err.stack);
 });
 
 //bot.on("message", message => {
