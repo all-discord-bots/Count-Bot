b471f3a3ce9fd1424397ad465f67b270b57b661b
BannerBomb <BannerBomb55@gmail.com>
2018-10-13 22:36:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ bot.on('messageDelete', (message) => {
 	//if (message.channel.type !== 'text') return;
 	//if (message.author.bot) return;
 	if (countingChannels.has(message.channel.id)) {
-		if (message.id !== message.channel.lastMessageID) {
+		if (message.id === message.channel.lastMessageID) {
 			//console.log(countingChannels.get(message.channel.id).getDeletedBy(message));
 			if (countingChannels.get(message.channel.id).getDeletedBy(message) === "bot") return;
 			/*message.channel.guild.roles.map((role) => role.id).forEach((value,index) => {
