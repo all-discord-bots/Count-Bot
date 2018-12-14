3f764b7fb1aaa555d8e84ecf147120e4aa717302
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2018-12-14 00:00:04 -0500

    Update and rename index.js(2) to index_new.js

--- index_new.js
+++ index_new.js
@@ -3,7 +3,7 @@
 const config = require('./config.json');
 //const fse = require('fs-extra');
 const { Client, Collection } = require('discord.js');
-const CountingChannelManager = require('./CountingChannelManager');
+const CountingChannelManager = require('./CountingChannelManager_new');
 
 const bot = global.bot = new Client({
 	messageCacheMaxSize: 10
