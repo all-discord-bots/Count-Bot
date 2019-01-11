f92b706832ec6d0b83b57d1d9d40a9576b860a77
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-11 18:27:21 -0500

    Update guildDelete.js

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -14,18 +14,7 @@ module.exports = class extends Event {
 					timestamp: new Date(),
 					description: `${guild.name} (${guild.id})\n\`${gusers} members   -   ${gbots} bots  (${Math.floor((gbots/guild.memberCount)*100)}%)\`\n\nOwner: ${guild.owner}  \`[${guild.owner.user.username}#${guild.owner.user.discriminator}]\``
 				}
-			}).catch((err) => console.error(err.toString()));
-			client.user.setPresence({
-				activity: {
-					name: `${this.client.guilds.size.toLocaleString()} server${this.client.guilds.size === 1 ? '' : 's'}`,
-					type: 3
-				},
-				status: 'online'
-			}).then(() => {
-				console.log(`Successfully updated the bots presence.`);
-			}).catch((err) => {
-				console.error(err.toString());
-			});
+			}).catch(console.error);
 			if (this.client.ready && guild.available && !this.client.options.preserveSettings) guild.settings.destroy().catch(() => null);
 		} catch (error) {
 			console.error(error.stack ? error.stack : error.toString());
