35fbef25c6ab8c090767261633f74cb89304b5a0
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-11 18:45:26 -0500

    Update TextChannelExtendables.js

--- src/extendables/TextChannelExtendables.js
+++ src/extendables/TextChannelExtendables.js
@@ -1,1 +1,18 @@
+const { Extendable } = require('klasa');
 
+module.exports = class extends Extendable {
+
+	constructor(...args) {
+		super(...args, { appliesTo: [String] });
+	}
+
+	static random(length = 1) {
+		const random13chars = () => {
+			return Math.random().toString(16).substring(2, 15)
+		}
+		return new Array(Math.ceil(length / 13)).fill(random13chars).reduce((string, func) => {
+			return string + func();
+		}, '').substring(0, length);
+	}
+
+}
