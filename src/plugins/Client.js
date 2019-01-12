a8321b7f97df6196616bb865fb37c6effe9f7e44
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-11 21:05:29 -0500

    Create Client.js

---
+++ src/plugins/Client.js
@@ -0,0 +1,24 @@
+const { Client, Schema, util: { mergeDefault } } = require('klasa');
+const { CLIENT } = require('./util/constants');
+
+const ChannelGateway = require('./ChannelGateway');
+Client.defaultChannelSchema = new Schema();
+
+module.exports = class extends Client {
+
+	constructor(options) {
+		super(options);
+		this.constructor[Client.plugin].call(this);
+	}
+
+	static [Client.plugin]() {
+		mergeDefault(CLIENT, this.options);
+		const { channels } = this.options.gateways;
+		const channelSchema = 'schema' in channels ? channels.schema : this.constructor.defaultChannelSchema;
+
+		this.gateways.members = new ChannelGateway(this.gateways, 'channels', channelSchema, channels.provider || this.options.providers.default);
+		this.gateways.keys.add('channels');
+		this.gateways._queue.push(this.gateways.channels.init.bind(this.gateways.channels));
+	}
+
+};
