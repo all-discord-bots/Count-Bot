0afe57882d22fed0accc17597258d3dcc439ec12
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-11 21:06:23 -0500

    Update Client.js

--- src/plugins/Client.js
+++ src/plugins/Client.js
@@ -1,7 +1,7 @@
 const { Client, Schema, util: { mergeDefault } } = require('klasa');
 const { CLIENT } = require('./util/constants');
 
-const ChannelGateway = require('./ChannelGateway');
+const ChannelGateway = require('./TextChannelGateway');
 Client.defaultChannelSchema = new Schema();
 
 module.exports = class extends Client {
