5637f316d0c2462f6d8b759938f0536eb153858e
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-13 03:26:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ bot.on('handleMessage', (message, action) => {
 		if (action === 'updated') {
 			if (message.id === message.channel.lastMessageID) {
 				bot.emit('giveMemberCantCount', message);
-				bot.emit('recalculateNumber', message);
+				bot.emit('recalculateNumber', message, 1);
 				return bot.emit('handleDelete', message);
 			} else {
 				return bot.emit('handleDelete', message);
