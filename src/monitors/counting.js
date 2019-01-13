8e7714e544c072068a5dae0c192eba4ab94368c0
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-12 23:12:05 -0500

    Update counting.js

--- src/monitors/counting.js
+++ src/monitors/counting.js
@@ -23,7 +23,8 @@ module.exports = class extends Monitor {
 	async run (message) {
 		try {
 			if (!message.guild) return;
-			if (message.guild && !message.guild.settings.countingChannels.includes(message.channel)) return;
+			const { settings: { countingChannels } } = message.guild;
+			if (!countingChannels.includes(message.channel) && !countingChannels.includes(message.channel.id)) return;
 			if (this.client.settings.blacklist.length && (this.client.settings.blacklist.includes(message.author.id) || this.client.settings.blacklist.includes(message.guild.id))) return;
 			this._beginCounting(message);
 		} catch (e) {
@@ -32,18 +33,46 @@ module.exports = class extends Monitor {
 	}
 	
 	async _beginCounting (message) {
-		const { channel: { settings: { maxMessups, currentNumber, countingBase, countBy } }, guild: { settings: { cantCountRole } } } = message;
-		
+		const { channel: { maxMessups, currentNumber, countingBase, countBy, currentNumber, lastNumber }, channel, guild: { settings: { cantCountRole } }, guild, member: { settings: { messups }, settings }, member } = message;
+		const number = this.parseNumber(message);
+		const last_number = lastNumber;
+		if ((message.content.length !== last_number.toString().length && number !== last_number) || !this.isNextInSequence(number) || message.attachments.size || message.embeds.length || RegExp(/([\s\+\=\_\`\~\!\@\#\$\%\^\&\*\(\)\\\|\]\[\{\}\'\"\;\:\?\/\,\<\>\t\r\na-z])/i).test(message.content) || RegExp(/(^(-0+|-{2,}))/i).test(message.content) || (message.content.length >= 2 && RegExp(/^(-?0+)/i).test(message.content)) || (!countBy.toString().startsWith('-') && message.content.includes('-')) || (countBy.toString().startsWith('-') && message.content.startsWith('-') && message.content.split('').filter((char) => char === '-').splice(1).length >= 1) || (!countBy.toString().includes('.') && message.content.includes('.')) || (countBy.toString().includes('.') && message.content.includes('.') && message.content.split('').filter((char) => char === '.').splice(1).length >= 1)) {
+			if (maxMessups !== Infinity) {
+				if (messups >= maxMessups) {
+					if (cantCountRole && channel.permissionsFor(guild.me).has('MANAGE_ROLES')) await member.roles.add(cantCountRole.id, 'This user can\'t count correctly');
+					await settings.reset('messups');
+				} else {
+					await settings.update('messups', messups + 1);
+				}
+			}
+			return this.deleteMessage(message);
+		}
+		currentNumber = this.currentCount(message);
+		return channel.sync();
 	}
 	
 	parseNumber(message) {
-		const { channel: { settings: { countBase, countBy } } = message;
+		const { channel: { countBase, countBy } } = message;
 		if (countBy % 1 !== 0)
 			return parseFloat(message.content);
 
 		if (message.content.search(/^\d+\.[1-9]/) !== -1)
 			return NaN;
 		return parseInt(message.content.replace(/ .*/, ''), countBase);
+	}
+	
+	isNextInSequence(message, number) {
+		return number === this.currentCount(message);
+	}
+	
+	currentCount(message) {
+		const { channel: { currentNumber, countBy } } = message;
+		return currentNumber + countBy;
+	}
+	
+	async deleteMessage(message) {
+		if (!message.channel.permissionsFor(message.guild.me).has('MANAGE_MESSAGES')) return;
+		await message.delete('Wrong number!');
 	}
 
 };
