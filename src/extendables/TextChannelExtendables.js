dd42a900a4b5ecb9b8dcbac3ecf93791f57793fc
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-12 20:11:29 -0500

    Update TextChannelExtendables.js

--- src/extendables/TextChannelExtendables.js
+++ src/extendables/TextChannelExtendables.js
@@ -5,10 +5,11 @@ module.exports = class extends Extendable {
 
 	constructor(...args) {
 		super(...args, { appliesTo: [TextChannel] });
-		this.currentNumber = this.currentNumber || 0;
-		this.maxMessups = this.maxMessups || 0;
-		this.countBase = this.countBase || 'decimal';
-		this.countBy = this.countBy || 1;
-		this.startAt = this.startAt || 0;
+		this.cached_settings = this.guild.settings.get('countingChannels').indexOf(this.id) < 0 ? undefined : this.guild.settings.get('countingChannels')[this.guild.settings.get('countingChannels').indexOf(this.id)];
+		this.currentNumber =  this.cached_settings.currentNumber || 0;
+		this.maxMessups = this.cached_settings.maxMessups || 0;
+		this.countBase = this.cached_settings.countBase || 'decimal';
+		this.countBy = this.cached_settings.countBy || 1;
+		this.startAt = this.cached_settings.startAt || 0;
 	}
 }
