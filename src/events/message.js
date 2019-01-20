f4fd86d890b6e46b39f034e9b300dc48553be777
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-20 04:49:43 -0500

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,13 +1,13 @@
 const { Event } = require('klasa');
 
 module.exports = class extends Event {
-	async run(msg) {
-		this.client.managers.stats.increment(`messages-${this.client.user === msg.author ? 'sent' : 'received'}`);
-		if (msg.mentions.has(this.client.user.id)) {
+	async run(message) {
+		this.client.managers.stats.increment(`messages-${this.client.user === message.author ? 'sent' : 'received'}`);
+		if (message.mentions.has(this.client.user.id)) {
 			this.client.managers.stats.increment('mentions');
 		}
 		try {
-			if (this.client.ready) this.client.monitors.run(msg);
+			if (this.client.ready) this.client.monitors.run(message);
 		} catch (error) {
 			console.error(error.stack ? error.stack : error.toString());
 		}
